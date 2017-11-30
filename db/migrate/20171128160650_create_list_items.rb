42225f7d75d4104c1f3162ea0778c8f79eee42a6
Hannah Hall <hannahkhall13@gmail.com>
2017-11-30 11:01:06 -0600

    migration and cors fix

--- db/migrate/20171128160650_create_list_items.rb
+++ db/migrate/20171128160650_create_list_items.rb
@@ -1,10 +1,10 @@
 class CreateListItems < ActiveRecord::Migration[5.1]
   def change
     create_table :list_items do |t|
-      t.references :list, foreign_key: true
-      t.string :title
-      t.text :description
-      t.boolean :is_done
+      t.references :list, foreign_key: true, null: false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.boolean :is_done, null: false, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128160525) do
+ActiveRecord::Schema.define(version: 20171128160650) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.integer "list_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.boolean "is_done", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_list_items_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
