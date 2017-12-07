fe150316e6bbcc4085db937c1675d8737632f0d7
Hannah Hall <hannahkhall13@gmail.com>
2017-12-07 14:32:46 -0600

    register method

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,18 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
 
+  def register
+    user = User.new({
+      email: params[:email], 
+      password: params[:password]
+    })
+    if user.save
+      authenticate
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :list_items
-  resources :lists
+  resources :lists do
+    resources :list_items
+  end
   resources :categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+  post 'register', to: 'authentication#register'
 end
